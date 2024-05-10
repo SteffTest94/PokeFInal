4764741e56db52e52205cae3831befaa6f1805d1
Your Name <you@example.com>
2024-05-09 22:23:17 -0500

    why must it be so annoying

--- pokemonList.js
+++ pokemonList.js
@@ -1,33 +1,24 @@
 const getPokemon = "https://pokeapi.co/api/v2/pokemon?limit=151";
 const dropDown = document.getElementById('pokelist');
-let addEle = document.createElement('option');
+const addEle = document.createElement('option');
 function createDropdown () {
     fetch (getPokemon)
     .then ( response =>
         response.json()
     )
     .then (function (allpokemon){
-        pokeName = ""
         allpokemon.results.forEach(
-            function fetchPokemonData(pokemon) {
-                let url = pokemon.url
-               // let pokeName = pokemon.name
-                fetch(url)
-                .then(response => response.json())
-                .then (function (pokeData){
-                    pokeName = pokeData.name
-                   // names = [pokeName]
-                    //names.push(pokeName)
-                    addEle.value = null
-                    addEle.text = pokeName
-                    console.log(addEle)
-                  //  for (i in names) {
-                        
-                  //  }
-                })
-                
+            function fetchPokemonName(pokemon) {
+                name: pokemon.name
             }
             )
+    for (i = 0; i < allpokemon.results.length; i++) {
+        addEle.text = allpokemon.results
+        addEle.value = +i
+        dropDown.add(addEle)
+        console.log(addEle)
+        console.log(allpokemon.results[i])
+    }
     }
     );
 };
