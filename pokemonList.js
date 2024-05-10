3842afdea338855e7388dc9dd495216fa7fdd790
Your Name <you@example.com>
2024-05-09 21:13:50 -0500

    I hate forEach and can't figure out how to make it do my bidding

--- pokemonList.js
+++ pokemonList.js
@@ -1,10 +1,34 @@
-import {results} from "./pokedex.json";
-let dropDown = document.getElementById('pokelist');
+const getPokemon = "https://pokeapi.co/api/v2/pokemon?limit=151";
+const dropDown = document.getElementById('pokelist');
 let addEle = document.createElement('option');
 function createDropdown () {
-for (let entry in {results}
-) {
-    addEle.text = entry.name;
-    dropDown.add(addEle);
-};};
+    fetch (getPokemon)
+    .then ( response =>
+        response.json()
+    )
+    .then (function (allpokemon){
+        pokeName = ""
+        allpokemon.results.forEach(
+            function fetchPokemonData(pokemon) {
+                let url = pokemon.url
+               // let pokeName = pokemon.name
+                fetch(url)
+                .then(response => response.json())
+                .then (function (pokeData){
+                    pokeName = pokeData.name
+                   // names = [pokeName]
+                    //names.push(pokeName)
+                    addEle.value = null
+                    addEle.text = pokeName
+                    console.log(addEle)
+                  //  for (i in names) {
+                        
+                  //  }
+                })
+                
+            }
+            )
+    }
+    );
+};
 createDropdown();
