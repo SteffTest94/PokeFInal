15d049c4fd006544be7a610fd7a7c30aac3d4fda
Your Name <you@example.com>
2024-05-09 23:23:53 -0500

    trying to recover

--- pokemonList.js
+++ pokemonList.js
@@ -1,10 +1,25 @@
-import {results} from "./pokedex.json";
-let dropDown = document.getElementById('pokelist');
-let addEle = document.createElement('option');
+const getPokemon = "https://pokeapi.co/api/v2/pokemon?limit=151";
+const dropDown = document.getElementById('pokelist');
+const addEle = document.createElement('option');
 function createDropdown () {
-for (let entry in {results}
-) {
-    addEle.text = entry.name;
-    dropDown.add(addEle);
-};};
+    fetch (getPokemon)
+    .then ( response =>
+        response.json()
+    )
+    .then (function (allpokemon){
+        allpokemon.results.forEach(
+            function fetchPokemonData(pokemon) {
+                name: pokemon.name
+                }
+            )
+    for (i in allpokemon.results) {
+    addEle.text = allpokemon.results[i].name
+    addEle.value = +i
+    dropDown.add(addEle)
+    console.log(addEle)
+    console.log(allpokemon.results[i])
+    }
+    }
+    );
+};
 createDropdown();
