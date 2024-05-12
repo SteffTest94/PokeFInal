3db8ce02dade7e46d572837568c98216061409bd
Your Name <you@example.com>
2024-05-12 11:26:10 -0500

    pulled full base code for reference

--- pokemonList.js
+++ pokemonList.js
@@ -1,7 +1,9 @@
 const getPokemon = "https://pokeapi.co/api/v2/pokemon?limit=151";
 const dropDown = document.getElementById('pokelist');
 const addEle = document.createElement('option');
+const selector = document.getElementById('pokemon');
 function createDropdown () {
+    let selector = document.getElementById('pokemon');
     fetch (getPokemon)
     .then ( response =>
         response.json()
@@ -13,13 +15,39 @@ function createDropdown () {
                 }
             )
     for (i = 0; i < allpokemon.results.length; i++) {
-    addEle.text = allpokemon.results[i].name
-    addEle.value = +i
-    dropDown.add(addEle)
-    console.log(addEle)
-    console.log(allpokemon.results.length)
+        addEle.text = allpokemon.results[i].name
+        addEle.value = 'pokemon'
+        dropDown.add(addEle)
+        console.log(addEle)
+        console.log(allpokemon.results.length)
     }
     }
     );
 };
+
+function pokeGrab() {
+    fetch (getPokemon)
+        .then ( response =>
+            response.json()
+        )
+        .then (function (allpokemon) {
+            pokeName = ""
+            allpokemon.results.forEach(
+                function fetchPokemonData(pokemon) {
+                    let url = pokemon.url
+                    let pokeName = pokemon.name
+                    fetch(url)
+                    .then(response => response.json())
+                    .then (function (pokeData){
+                        console.log(pokeData)
+                        //addEle.text = pokeName;
+                        //dropDown.add(addEle);
+                    })
+                    
+                }
+                ) 
+        }
+        );
+};
 createDropdown();
+selector.onselect(pokeGrab());
