395fc81aab27a2d40c4f1c79b1940f9639289db0
Your Name <you@example.com>
2024-05-12 14:51:41 -0500

    i know there's a way to pull the data, I just know it

--- pokemonList.js
+++ pokemonList.js
@@ -2,19 +2,69 @@ const getPokemon = "https://pokeapi.co/api/v2/pokemon?limit=151";
 const dropDown = document.querySelector('#pokelist');
 const addEle = document.createElement('option');
 const selector = document.getElementById('pokemon');
-function createDropdown () {
 
-    fetch (getPokemon)
-    .then ( response =>
-        response.json()
-    )
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+let allPokemonContainer = document.querySelector('#poke-container')
+let generateBtn = document.querySelector('#generate-pokemon');
+    generateBtn.addEventListener('click', renderEverything)
+
+function renderEverything(){
+    let allPokemonContainer = document.querySelector('#poke-container')
+    allPokemonContainer.innerText = "";
+    fetchPokemon();
+}
+function fetchPokemon(){
+    fetch(getPokemon)
+    .then(response => response.json())
     .then(function(allpokemon){
         allpokemon.results.forEach(function(pokemon){
             addEle.text = pokemon.name
             addEle.value = 'pokemon'
             dropDown.append(addEle)
-            console.log(addEle);
+            console.log(addEle)
+            fetchPokemonData(pokemon);
         })
     })
-};
+}
+
+function fetchPokemonData(pokemon){
+    let url = pokemon.url 
+    fetch(url)
+    .then(response => response.json())
+    .then(function(pokeData){
+        renderPokemon(pokeData)
+    })
+}
+
+function renderPokemon(pokeData){
+    let allPokemonContainer = document.getElementById('poke-container');
+    let pokeContainer = document.createElement("div") //div will be used to hold the data/details for indiviual pokemon.{}
+    let pokeName = document.createElement('h4')
+    pokeName.innerText = pokeData.name
+    let pokeNumber = document.createElement('p')
+    pokeNumber.innerText = `#${pokeData.id}`
+    let pokeTypes = document.createElement('ul') 
+    //ul list will hold the pokemon types
+    createTypes(pokeData.types, pokeTypes) 
+    // helper function to go through the types array and create li tags for each one
+    pokeContainer.append(pokeName, pokeNumber, pokeTypes);   
+    //appending all details to the pokeContainer div
+    allPokemonContainer.appendChild(pokeContainer);       
+    //appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards
+}
+
+function createTypes(types, ul){
+    types.forEach(function(type){
+        let typeLi = document.createElement('li');
+        typeLi.innerText = type['type']['name'];
+        ul.append(typeLi)
+    })
+}
+
+
+function createDropdown () {
+}
 createDropdown()
+})
